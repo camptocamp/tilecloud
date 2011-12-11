0f6fe5471b17da531f8b7f1a773bc128577a8bb0
Tom Payne <twpayne@gmail.com>
2011-12-11 23:15:31 +0100

    Add FilesystemTileStore.get_all and list

--- tilecloud.py
+++ tilecloud.py
@@ -372,11 +372,26 @@ class FilesystemTileStore(TileStore):
             os.remove(filename)
         return tile
 
+    def get_all(self):
+        for tile in self.list():
+            with open(tile.path) as file:
+                tile.data = file.read()
+            yield tile
+
     def get_one(self, tile):
         filename = self.tile_layout.filename(tile.tilecoord)
         with open(filename) as file:
             tile.data = file.read()
         return tile
+
+    def list(self):
+        top = getattr(self.tile_layout, 'prefix', '.')
+        for dirpath, dirnames, filenames in os.walk(top):
+            for filename in filenames:
+                path = os.path.join(dirpath, filename)
+                tilecoord = self.tile_layout.tilecoord(path)
+                if tilecoord:
+                    yield Tile(tilecoord, path=path)
 
     def put_one(self, tile):
         filename = self.tile_layout.filename(tile.tilecoord)
