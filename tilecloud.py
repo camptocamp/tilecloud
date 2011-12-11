bec2cd583c75a6459f3eee1607c2fc8f6afa6dae
Tom Payne <twpayne@gmail.com>
2011-12-11 10:26:17 +0100

    Move quadcode functions into I3DTileLayout

--- tilecloud.py
+++ tilecloud.py
@@ -50,31 +50,11 @@ class TileCoord(object):
         self.x = x
         self.y = y
 
-    def quadcode(self):
-        x, y = self.x, self.y
-        result = ''
-        for i in xrange(0, self.z):
-            result += '0123'[(x & 1) + ((y & 1) << 1)]
-            x >>= 1
-            y >>= 1
-        return result[::-1]
-
     def __repr__(self):
         return '%s(%r, %r, %r)' % (self.__class__.__name__, self.z, self.x, self.y)
 
     def __str__(self):
         return '%d/%d/%d' % (self.z, self.x, self.y)
-
-    @classmethod
-    def from_quadcode(cls, quadcode):
-        z, x, y = len(quadcode), 0, 0
-        for i, c in enumerate(quadcode):
-            mask = 1 << (z - i - 1)
-            if c == '1' or c == '3':
-                x |= mask
-            if c == '2' or c == '3':
-                y |= mask
-        return cls(z, x, y)
 
 
 
@@ -128,10 +108,31 @@ class I3DTileLayout(TileLayout):
         TileLayout.__init__(self, self.PATTERN, self.RE)
 
     def filename(self, tilecoord):
-        return '/'.join(re.findall(r'[0-3]{1,2}', tilecoord.quadcode()))
+        return '/'.join(re.findall(r'[0-3]{1,2}', I3DTileLayout.quadcode_from_tilecoord(tilecoord)))
 
     def _tilecoord(self, match):
-        return TileCoord.from_quadcode(re.sub(r'/', '', match.group()))
+        return I3DTileLayout.tilecoord_from_quadcode(re.sub(r'/', '', match.group()))
+
+    @staticmethod
+    def quadcode_from_tilecoord(tilecoord):
+        x, y = tilecoord.x, tilecoord.y
+        result = ''
+        for i in xrange(0, tilecoord.z):
+            result += '0123'[(x & 1) + ((y & 1) << 1)]
+            x >>= 1
+            y >>= 1
+        return result[::-1]
+
+    @staticmethod
+    def tilecoord_from_quadcode(quadcode):
+        z, x, y = len(quadcode), 0, 0
+        for i, c in enumerate(quadcode):
+            mask = 1 << (z - i - 1)
+            if c == '1' or c == '3':
+                x |= mask
+            if c == '2' or c == '3':
+                y |= mask
+        return TileCoord(z, x, y)
 
 
 
