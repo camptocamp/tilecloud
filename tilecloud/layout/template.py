def07c80d058ecf8d807ae9f4827b5829dfeee5f
Tom Payne <twpayne@gmail.com>
2011-12-14 10:14:38 +0100

    Add TemplateTileLayout

---
+++ tilecloud/layout/template.py
@@ -0,0 +1,30 @@
+import re
+
+from tilecloud import TileCoord, TileLayout
+
+
+
+class TemplateTileLayout(TileLayout):
+
+    def __init__(self, template):
+        self.template = template
+        index, patterns, filename_patterns = 0, [], []
+        for match in re.finditer(r'%\(([xyz])\)d', self.template):
+            prematch_pattern = re.escape(self.template[index:match.start()])
+            patterns.append(prematch_pattern)
+            patterns.append(r'\d+')
+            filename_patterns.append(prematch_pattern)
+            filename_patterns.append(r'(?P<%s>\d+)' % match.group(1))
+            index = match.end()
+        postmatch_pattern = re.escape(self.template[index:])
+        patterns.append(postmatch_pattern)
+        filename_patterns.append(postmatch_pattern)
+        pattern = ''.join(patterns)
+        filename_re = re.compile(''.join(filename_patterns))
+        TileLayout.__init__(self, pattern, filename_re)
+
+    def filename(self, tilecoord):
+        return self.template % dict(z=tilecoord.z, x=tilecoord.x, y=tilecoord.y)
+
+    def _tilecoord(self, match):
+        return TileCoord(*(int(match.group(s)) for s in 'zxy'))
