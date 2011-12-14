985de3bae97586c91fd9cb2032b14069e32c0cb7
Tom Payne <twpayne@gmail.com>
2011-12-14 18:01:37 +0100

    Replace debug.py with debug_black.py and debug_white.py

---
+++ debug_black.py
@@ -0,0 +1,4 @@
+from tilecloud.store.debug import DebugTileStore
+
+
+tile_store = DebugTileStore(color=(0, 0, 0))
--- debug_white.py
+++ debug_white.py
@@ -1,4 +1,4 @@
 from tilecloud.store.debug import DebugTileStore
 
 
-tile_store = DebugTileStore()
+tile_store = DebugTileStore(color=(1, 1, 1))
