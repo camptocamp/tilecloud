2d65c29d545aa8e5d15388d2564d50a9b9668eae
fredj <frederic.junod@camptocamp.com>
2011-12-14 11:34:04 +0100

    Guess content type from data

--- tilecloud/filter/contenttype.py
+++ tilecloud/filter/contenttype.py
@@ -1,9 +1,17 @@
 class ContentTypeAdder(object):
     """A class that adds a content type to a tile"""
 
-    def __init__(self, content_type):
+    def __init__(self, content_type=None):
         self.content_type = content_type
 
     def __call__(self, tile):
-        tile.content_type = self.content_type
+        if self.content_type is None:
+            if hasattr(tile, 'data'):
+                data = str(tile.data)
+                if data.startswith('\x89PNG\x0d\x0a\x1a\x0a'):
+                    tile.content_type = 'image/png'
+                elif data.startswith('\xff\xd8'):
+                    tile.content_type = 'image/jpeg'
+        else:
+            tile.content_type = self.content_type
         return tile
