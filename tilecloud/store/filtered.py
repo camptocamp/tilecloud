2b38352e56f7120f1064531bebfb6edb4e6b6cce
Tom Payne <twpayne@gmail.com>
2011-12-14 17:55:19 +0100

    Add FilteredTileStore

---
+++ tilecloud/store/filtered.py
@@ -0,0 +1,12 @@
+from tilecloud import TileStore
+
+
+
+class FilteredTileStore(TileStore):
+
+    def __init__(self, tile_store, filters):
+        self.tile_store = tile_store
+        self.filters = filters
+
+    def get_one(self, tile):
+        return reduce(lambda tile, filter: filter(tile), self.filters, self.tile_store.get_one(tile))
