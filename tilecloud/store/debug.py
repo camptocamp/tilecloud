dc6d612c9409c319bf1d913785baae981263da33
Tom Payne <tom.payne@camptocamp.com>
2011-12-14 17:03:42 +0100

    Add transparent and more beautiful debug tiles

--- tilecloud/store/debug.py
+++ tilecloud/store/debug.py
@@ -8,22 +8,35 @@ from tilecloud import TileStore
 
 class DebugTileStore(TileStore):
 
-    def __init__(self, inset=4):
-        self.inset = 4
+    def __init__(self, color=(0, 0, 0), transparent=True):
+        self.color = color
+        self.inverse_color = tuple(1.0 - x for x in color)
+        self.transparent = transparent
 
     def get_one(self, tile):
-        image_surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 256, 256)
+        format = cairo.FORMAT_ARGB32 if self.transparent else cairo.FORMAT_RGB24
+        image_surface = cairo.ImageSurface(format, 256, 256)
         context = cairo.Context(image_surface)
-        context.set_source_rgb(1, 1, 1)
-        context.paint()
-        context.rectangle(0.5 + self.inset, 0.5 + self.inset, 256 - 2 * self.inset, 256 - 2 * self.inset)
-        context.set_source_rgb(0, 0, 0)
+        if not self.transparent:
+            context.set_source_rgba(1, 1, 1, 1)
+            context.paint()
+        context.rectangle(0.5, 0.5, 256, 256)
+        if self.transparent:
+            context.set_source_rgb(*self.inverse_color)
+            context.set_line_width(2)
+            context.stroke_preserve()
+        context.set_source_rgb(*self.color)
         context.set_line_width(1)
         context.stroke()
         text = str(tile.tilecoord)
         extents = context.text_extents(text)
         context.move_to(128.0 - extents[2] / 2.0, 128 - extents[3] / 2.0)
-        context.show_text(text)
+        context.text_path(text)
+        if self.transparent:
+            context.set_source_rgb(*self.inverse_color)
+            context.set_line_width(2)
+            context.stroke_preserve()
+        context.set_source_rgb(*self.color)
         context.fill()
         string_io = StringIO()
         image_surface.write_to_png(string_io)
