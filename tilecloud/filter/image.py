9d0910c39b52627467582a5ea001756810534b17
Frederic Junod <frederic.junod@camptocamp.com>
2011-12-14 14:21:12 +0100

    content_type_to_pil_format function

--- tilecloud/filter/image.py
+++ tilecloud/filter/image.py
@@ -3,6 +3,14 @@ from cStringIO import StringIO
 import PIL.Image
 
 
+def content_type_to_pil_format(content_type):
+    if content_type == 'image/jpeg':
+        return 'JPEG'
+    elif content_type == 'image/png':
+        return 'PNG'
+    else:
+        assert False
+
 
 class ImageFormatConverter(object):
     """A class that converts a tile into the desired format"""
@@ -10,12 +18,7 @@ class ImageFormatConverter(object):
     def __init__(self, content_type, **kwargs):
         self.content_type = content_type
         self.kwargs = kwargs
-        if self.content_type == 'image/jpeg':
-            self.format = 'JPEG'
-        elif content_type == 'image/png':
-            self.format = 'PNG'
-        else:
-            assert False
+        self.format = content_type_to_pil_format(self.content_type)
 
     def __call__(self, tile):
         if not hasattr(tile, 'content_type') or tile.content_type != self.content_type:
@@ -33,16 +36,9 @@ class PILImageFilters(object):
 
     def __call__(self, tile):
         if hasattr(tile, 'data'):
-            im = PIL.Image.open(StringIO(tile.data))
-            im = im.filter(self.filter)
-            if tile.content_type == 'image/png':
-                format = 'PNG'
-            elif tile.content_type == 'image/jpeg':
-                format = 'JPEG'
-            else:
-                assert False
+            im = PIL.Image.open(StringIO(tile.data)).filter(self.filter)
             output = StringIO()
-            im.save(output, format)
+            im.save(output, content_type_to_pil_format(tile.content_type))
             tile.data = output.getvalue()
             output.close()
             return tile
