b3062b10f093ec20742480a930ff5f7b6b3bedcc
Tom Payne <tom.payne@camptocamp.com>
2011-12-21 12:19:49 +0100

    Add MergeFilter

--- tilecloud/filter/image.py
+++ tilecloud/filter/image.py
@@ -2,6 +2,8 @@ from cStringIO import StringIO
 
 import PIL.Image
 
+from tilecloud import Tile
+
 
 
 FORMAT_BY_CONTENT_TYPE = {
@@ -25,6 +27,31 @@ class ImageFormatConverter(object):
             PIL.Image.open(StringIO(tile.data)).save(string_io, self.format, **self.kwargs)
             tile.content_type = self.content_type
             tile.data = string_io.getvalue()
+        return tile
+
+
+
+class MergeFilter(object):
+
+    def __init__(self, tile_stores, content_type=None, **kwargs):
+        self.tile_stores = list(tile_stores)
+        self.content_type = content_type
+        self.kwargs = kwargs
+
+    def __call__(self, tile):
+        image = PIL.Image.open(StringIO(tile.data))
+        for tile_store in self.tile_stores:
+            t = tile_store.get_one(Tile(tile.tilecoord))
+            if t is not None:
+                image2 = PIL.Image.open(StringIO(t.data))
+                image.paste(image2, None, image2)
+        content_type = self.content_type
+        if content_type is None:
+            self.content_type = tile.content_type
+        string_io = StringIO()
+        image.save(string_io, FORMAT_BY_CONTENT_TYPE[content_type], **self.kwargs)
+        tile.content_type = content_type
+        tile.data = string_io.getvalue()
         return tile
 
 
