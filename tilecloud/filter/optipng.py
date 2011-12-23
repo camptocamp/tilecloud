073135d4df66740a6c7c5b019643b769e3e7d5cc
Tom Payne <twpayne@gmail.com>
2011-12-23 18:57:16 +0100

    Add OptiPNG filter

---
+++ tilecloud/filter/optipng.py
@@ -0,0 +1,26 @@
+import os
+from subprocess import call
+from tempfile import NamedTemporaryFile
+
+
+
+class OptiPNG(object):
+
+    def __init__(self, options, arg0='/usr/bin/optipng'):
+        self.args = [arg0, '-q'] + list(options)
+
+    def __call__(self, tile):
+        ntf = NamedTemporaryFile(delete=False, suffix='.png')
+        try:
+            ntf.write(tile.data)
+            ntf.close()
+            retcode = call(self.args + [ntf.name])
+            if retcode == 0:
+                with open(ntf.name) as f:
+                    tile.data = f.read()
+        finally:
+            try:
+                os.unlink(ntf.name)
+            except IOError:
+                pass
+        return tile
