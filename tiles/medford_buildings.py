9208462d9642d30206948a16882d857424bd0c28
Tom Payne <twpayne@gmail.com>
2012-03-19 19:59:51 +0100

    Add WMTS demo

---
+++ tiles/medford_buildings.py
@@ -0,0 +1,10 @@
+from tilecloud.store.wmts import WMTSTileStore
+
+
+tile_store = WMTSTileStore(
+        url='http://v2.suite.opengeo.org/geoserver/gwc/service/wmts/',
+        layer='medford:buildings',
+        style='_null',
+        format='image/png',
+        tile_matrix_set='EPSG:900913',
+        tile_matrix=lambda z: 'EPSG:900913:%d' % (z,))
