36c39a495a99caa4889e5831c57d7f5179712539
Tom Payne <twpayne@gmail.com>
2012-03-28 17:17:08 +0200

    Correct content type of Stamen tiles

--- tiles/stamen_com/terrain.py
+++ tiles/stamen_com/terrain.py
@@ -5,4 +5,4 @@ from tilecloud.store.url import URLTileStore
 tile_store = URLTileStore(
         (TemplateTileLayout('http://%s.tile.stamen.com/terrain/%%(z)d/%%(x)d/%%(y)d.jpg' % server) for server in 'abcd'),
         attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
-        content_type='image/png')
+        content_type='image/jpg')
