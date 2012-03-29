b05facf8656c13ca6c610d4f2555f5f68424330d
Tom Payne <twpayne@gmail.com>
2012-03-29 09:54:49 +0200

    Add NullTileStore

---
+++ tilecloud/store/null.py
@@ -0,0 +1,20 @@
+from tilecloud import TileStore
+
+
+class NullTileStore(TileStore):
+    """A TileStore that does nothing"""
+
+    def __contains__(self, tile):
+        return False
+
+    def delete_one(self, tile):
+        return tile
+
+    def get_one(self, tile):
+        return tile
+
+    def list(self, tile):
+        return ()
+
+    def put_one(self, tile):
+        return tile
