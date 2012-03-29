5a1c2e1569ef8773d1d39dfdc873986d84eb3acc
Tom Payne <twpayne@gmail.com>
2012-03-29 09:55:09 +0200

    Add shortcut for creating null tile stores

--- tilecloud/__init__.py
+++ tilecloud/__init__.py
@@ -348,6 +348,9 @@ class TileStore(object):
 
     @classmethod
     def load(cls, name):
+        if name == 'null://':
+            from tilecloud.store.null import NullTileStore
+            return NullTileStore()
         if name.startswith('http://') or name.startswith('https://'):
             from tilecloud.layout.template import TemplateTileLayout
             from tilecloud.store.url import URLTileStore
