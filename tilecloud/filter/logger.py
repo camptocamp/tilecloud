930c2d11c31b4e67048fda988500aec727f84756
Tom Payne <twpayne@gmail.com>
2012-04-06 00:02:35 +0200

    Handle None tiles

--- tilecloud/filter/logger.py
+++ tilecloud/filter/logger.py
@@ -8,9 +8,10 @@ class Logger(object):
         self.kwargs = kwargs
 
     def __call__(self, tile):
-        variables = dict()
-        variables.update(tile.__dict__)
-        variables.update(tile.tilecoord.__dict__)
-        self.logger.log(self.level, self.msgformat % variables,
-                        *self.args, **self.kwargs)
+        if tile is not None:
+            variables = dict()
+            variables.update(tile.__dict__)
+            variables.update(tile.tilecoord.__dict__)
+            self.logger.log(self.level, self.msgformat % variables,
+                            *self.args, **self.kwargs)
         return tile
