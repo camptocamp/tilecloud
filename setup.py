7da3567dce34f941220ff16da6e1938fe9722194
Tom Payne <twpayne@gmail.com>
2012-04-09 15:40:44 +0200

    Add inital setup script

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+install_requires = [
+        'bottle',
+        'pyproj',
+        ]
+
+setup_requires = [
+        'nose',
+        ]
+
+tests_require = install_requires = [
+        'coverage',
+        ]
+
+setup(
+        name='tilecloud',
+        version=version,
+        description='Tools for managing tiles',
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+        ],
+        author='Tom Payne',
+        author_email='twpayne@gmail.com',
+        url='http://github.com/twpayne/tilecloud',
+        license='BSD',
+        packages=find_packages(exclude=['tiles']),
+        zip_safe=True,
+        install_requires=install_requires,
+        setup_requires=setup_requires,
+        tests_require=tests_require,
+        test_suite='tilecloud.tests',
+        entry_points="""
+        # -*- Entry points: -*-
+        """,
+        long_description=README,
+        )
