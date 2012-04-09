89e58ff998140bf06f316b3eaab33704fdca187b
Tom Payne <twpayne@gmail.com>
2012-04-09 17:31:00 +0200

    Extend LogTileStore to write files

--- tilecloud/store/log.py
+++ tilecloud/store/log.py
@@ -5,21 +5,25 @@ from tilecloud import Tile, TileStore
 
 
 class LogTileStore(TileStore):
-    """Generates all tile coordinates matching the specified layout from lines"""
+    """Generates all tile coordinates matching the specified layout from file"""
 
-    def __init__(self, tile_layout, lines=None, **kwargs):
+    def __init__(self, tile_layout, file=None, **kwargs):
         TileStore.__init__(self, **kwargs)
         self.tile_layout = tile_layout
-        self.lines = lines
+        self.file = file
 
     def get_one(self, tile):
         tile.data = None
         return tile
 
     def list(self):
-        # FIXME warn that this consumes lines
+        # FIXME warn that this consumes file
         filename_re = re.compile(self.tile_layout.pattern)
-        for line in self.lines:
+        for line in self.file:
             match = filename_re.search(line)
             if match:
                 yield Tile(self.tile_layout.tilecoord(match.group()), line=line)
+
+    def put_one(self, tile):
+        self.file.write(self.tile_layout.filename(tile.tilecoord) + '\n')
+        return tile
