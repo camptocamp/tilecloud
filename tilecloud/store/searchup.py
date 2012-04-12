a1badf2768d7b706ef8250a4dededde93d9743be
Tom Payne <twpayne@gmail.com>
2012-04-12 23:43:27 +0200

    Add SearchUpTileStore

---
+++ tilecloud/store/searchup.py
@@ -0,0 +1,14 @@
+from tilecloud import TileStore
+
+
+class SearchUpTileStore(TileStore):
+
+    def __init__(self, tile_store):
+        self.tile_store = tile_store
+
+    def __call__(self, tile):
+        while tile:
+            if tile in self.tile_store:
+                return tile
+            tile = tile.parent()
+        return None
