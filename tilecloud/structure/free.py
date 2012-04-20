609e4f1bc6df600a0ccd70ff8918fa1391a54eae
Tom Payne <twpayne@gmail.com>
2012-04-21 00:07:47 +0200

    Add initial FreeTileStructure implementation

---
+++ tilecloud/structure/free.py
@@ -0,0 +1,52 @@
+from tilecloud import TileCoord, TileStructure
+
+
+class FreeTileStructure(TileStructure):
+
+    def __init__(self, resolutions, max_extent=None):
+        assert list(resolutions) == sorted(resolutions, reverse=True)
+        self.resolutions = resolutions
+        self.max_extent = max_extent
+        self.root_zs = []
+        self.parent_zs = []
+        self.child_zs = []
+        for i, resolution in enumerate(self.resolutions):
+            for parent in xrange(i - 1, -1, -1):
+                if self.resolutions[parent] % resolution == 0:
+                    self.parent_zs.append(parent)
+                    self.child_zs[parent].append(i)
+                    break
+            else:
+                self.root_zs.append(i)
+                self.parent_zs.append(None)
+            self.child_zs.append([])
+
+    def children(self, tilecoord):
+        if tilecoord.z < len(self.resolutions):
+            for child_z in self.child_zs[tilecoord.z]:
+                factor = self.resolutions[tilecoord.z] / self.resolutions[child_z]
+                for i in xrange(0, factor):
+                    x = factor * tilecoord.x + i
+                    for j in xrange(0, factor):
+                        y = factor * tilecoord.y + j
+                        yield TileCoord(child_z, x, y)
+
+    def parent(self, tilecoord):
+        parent_z = self.parent_zs[tilecoord.z]
+        if parent_z is None:
+            return None
+        else:
+            factor = self.resolutions[parent_z] / self.resolutions[tilecoord.z]
+            return TileCoord(parent_z, int(tilecoord.x // factor), int(tilecoord.y // factor))
+
+    def roots(self):
+        for root_z in self.root_zs:
+            x, s = 0, 0
+            while s < self.resolutions[0]:
+                y, t = 0, 0
+                while t < self.resolutions[0]:
+                    yield TileCoord(root_z, x, y)
+                    y += 1
+                    t += self.resolutions[root_z]
+                x += 1
+                s += self.resolutions[root_z]
--- tilecloud/tests/test_tilestructure.py
+++ tilecloud/tests/test_tilestructure.py
@@ -1,7 +1,50 @@
+from collections import defaultdict
 import unittest
 
 from tilecloud import TileCoord
+from tilecloud.structure.free import FreeTileStructure
 from tilecloud.structure.quad import QuadTileStructure
+
+
+class TestFreeTileStructure(unittest.TestCase):
+
+    def setUp(self):
+        self.resolutions = (1000, 999, 500, 250, 125, 123, 111, 100, 50, 25, 10)
+        self.fts = FreeTileStructure(self.resolutions)
+
+    def test_factors(self):
+        for i, resolution in enumerate(self.resolutions):
+            for child_z in self.fts.child_zs[i]:
+                self.assertEquals(self.resolutions[i] % self.resolutions[child_z], 0)
+
+    def test_root_parents(self):
+        for root_z in set(root.z for root in self.fts.roots()):
+            self.assertEquals(self.fts.parent(TileCoord(root_z, 0, 0)), None)
+
+    def test_root_zero(self):
+        self.assertEquals(self.fts.parent(TileCoord(0, 0, 0)), None)
+
+
+class TestFreeQuadTileStructureEquivalence(unittest.TestCase):
+
+    def setUp(self):
+        self.fts = FreeTileStructure([16, 8, 4, 2])  # FIXME resolutions of 1 :-)
+        self.qts = QuadTileStructure(max_zoom=3)
+
+    def test_children(self):
+        tc = TileCoord(2, 2, 3)
+        self.assertEqual(sorted(self.fts.children(tc)), sorted(self.qts.children(tc)))
+
+    def test_children_root(self):
+        tc = TileCoord(0, 0, 0)
+        self.assertEqual(sorted(self.fts.children(tc)), sorted(self.qts.children(tc)))
+
+    def test_parent(self):
+        tc = TileCoord(3, 3, 5)
+        self.assertEqual(self.fts.parent(tc), self.qts.parent(tc))
+
+    def test_roots(self):
+        self.assertEqual(list(self.fts.roots()), list(self.qts.roots()))
 
 
 class TestQuadTileStructure(unittest.TestCase):
