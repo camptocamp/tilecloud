49e559ac18e8cdddb1943b367599e18e8ac1310b
Tom Payne <twpayne@gmail.com>
2012-04-24 17:52:50 +0200

    Rename MetaTileToTileStore to MetaTileSplitterTileStore and add tests

--- tilecloud/store/metatile.py
+++ tilecloud/store/metatile.py
@@ -5,23 +5,21 @@ from PIL import Image
 from tilecloud import Tile, TileStore
 
 
-class MetaTileToTileStore(TileStore):
+class MetaTileSplitterTileStore(TileStore):
 
-    def __init__(self, tile_size, buffer, image_format, **kwargs):
+    def __init__(self, format, tile_size=256, border=0, **kwargs):
+        self.format = format
         self.tile_size = tile_size
-        self.buffer = buffer
-        self.image_format = image_format
+        self.border = border
         TileStore.__init__(self, **kwargs)
 
     def get(self, tiles):
         for metatile in tiles:
             metaimage = Image.open(StringIO(metatile.data))
             for tilecoord in metatile.tilecoord:
-                x = tilecoord.x - metatile.tilecoord.x
-                y = tilecoord.y - metatile.tilecoord.y
-                px_x = self.buffer + x * self.tile_size
-                px_y = self.buffer + y * self.tile_size
-                image = metaimage.crop((px_x, px_y, px_x + self.tile_size, px_y + self.tile_size))
+                x = self.border + (tilecoord.x - metatile.tilecoord.x) * self.tile_size
+                y = self.border + (tilecoord.y - metatile.tilecoord.y) * self.tile_size
+                image = metaimage.crop((x, y, x + self.tile_size, y + self.tile_size))
                 string_io = StringIO()
-                image.save(string_io, self.image_format)
+                image.save(string_io, self.format)
                 yield Tile(tilecoord, data=string_io.getvalue())
---
+++ tilecloud/tests/test_metatile.py
@@ -0,0 +1,75 @@
+from cStringIO import StringIO
+import unittest
+
+from PIL import Image
+
+from tilecloud import MetaTileCoord, Tile, TileCoord
+from tilecloud.store.metatile import MetaTileSplitterTileStore
+
+
+class TestMetaTileSplitterTileStore(unittest.TestCase):
+
+    def setUp(self):
+        self.mtsts = MetaTileSplitterTileStore('PNG', tile_size=2)
+
+    def test_get(self):
+        image = Image.new('RGBA', (4, 4))
+        image.paste((255, 0, 0, 0), (0, 0, 2, 2))
+        image.paste((0, 255, 0, 0), (0, 2, 2, 4))
+        image.paste((0, 0, 255, 0), (2, 0, 4, 2))
+        image.paste((0, 0, 0, 255), (2, 2, 4, 4))
+        string_io = StringIO()
+        image.save(string_io, 'PNG')
+        tile = Tile(MetaTileCoord(2, 1, 0, 0), data=string_io.getvalue())
+        tiles = list(self.mtsts.get([tile]))
+        self.assertEqual(len(tiles), 4)
+        self.assertEqual(tiles[0].tilecoord, TileCoord(1, 0, 0))
+        image = Image.open(StringIO(tiles[0].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (255, 0, 0, 0))])
+        self.assertEqual(tiles[1].tilecoord, TileCoord(1, 0, 1))
+        image = Image.open(StringIO(tiles[1].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (0, 255, 0, 0))])
+        self.assertEqual(tiles[2].tilecoord, TileCoord(1, 1, 0))
+        image = Image.open(StringIO(tiles[2].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (0, 0, 255, 0))])
+        self.assertEqual(tiles[3].tilecoord, TileCoord(1, 1, 1))
+        image = Image.open(StringIO(tiles[3].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (0, 0, 0, 255))])
+
+
+class TestMetaTileSplitterTileStoreWithBorder(unittest.TestCase):
+
+    def setUp(self):
+        self.mtsts = MetaTileSplitterTileStore('PNG', tile_size=2, border=2)
+
+    def test_get(self):
+        image = Image.new('RGBA', (8, 8))
+        image.paste((255, 0, 0, 0), (0, 0, 4, 4))
+        image.paste((0, 255, 0, 0), (0, 4, 4, 8))
+        image.paste((0, 0, 255, 0), (4, 0, 8, 4))
+        image.paste((0, 0, 0, 255), (4, 4, 8, 8))
+        string_io = StringIO()
+        image.save(string_io, 'PNG')
+        tile = Tile(MetaTileCoord(2, 1, 0, 0), data=string_io.getvalue())
+        tiles = list(self.mtsts.get([tile]))
+        self.assertEqual(len(tiles), 4)
+        self.assertEqual(tiles[0].tilecoord, TileCoord(1, 0, 0))
+        image = Image.open(StringIO(tiles[0].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (255, 0, 0, 0))])
+        self.assertEqual(tiles[1].tilecoord, TileCoord(1, 0, 1))
+        image = Image.open(StringIO(tiles[1].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (0, 255, 0, 0))])
+        self.assertEqual(tiles[2].tilecoord, TileCoord(1, 1, 0))
+        image = Image.open(StringIO(tiles[2].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (0, 0, 255, 0))])
+        self.assertEqual(tiles[3].tilecoord, TileCoord(1, 1, 1))
+        image = Image.open(StringIO(tiles[3].data))
+        self.assertEqual(image.size, (2, 2))
+        self.assertEqual(image.getcolors(), [(4, (0, 0, 0, 255))])
