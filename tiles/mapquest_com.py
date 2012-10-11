5ea453eeec2b911da45a82aecb9cd081011250cb
Tom Payne <twpayne@gmail.com>
2012-10-11 10:22:53 +0200

    PEP8 indentation fixes

--- tiles/mapquest_com.py
+++ tiles/mapquest_com.py
@@ -3,6 +3,6 @@ from tilecloud.store.url import URLTileStore
 
 
 tilestore = URLTileStore(
-        (TemplateTileLayout('http://otile%d.mqcdn.com/tiles/1.0.0/osm/%%(z)d/%%(x)d/%%(y)d.png' % i) for i in xrange(1, 5)),
-        attribution='Data, imagery and map information provided by MapQuest, <a href="http://www.openstreetmap.org/">Open Street Map</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.',
-        content_type='image/png')
+    (TemplateTileLayout('http://otile%d.mqcdn.com/tiles/1.0.0/osm/%%(z)d/%%(x)d/%%(y)d.png' % i) for i in xrange(1, 5)),
+    attribution='Data, imagery and map information provided by MapQuest, <a href="http://www.openstreetmap.org/">Open Street Map</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.',
+    content_type='image/png')
--- tiles/stamen_com/toner.py
+++ tiles/stamen_com/toner.py
@@ -3,6 +3,6 @@ from tilecloud.store.url import URLTileStore
 
 
 tilestore = URLTileStore(
-        (TemplateTileLayout('http://%s.tile.stamen.com/toner/%%(z)d/%%(x)d/%%(y)d.png' % server) for server in 'abcd'),
-        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
-        content_type='image/png')
+    (TemplateTileLayout('http://%s.tile.stamen.com/toner/%%(z)d/%%(x)d/%%(y)d.png' % server) for server in 'abcd'),
+    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+    content_type='image/png')
