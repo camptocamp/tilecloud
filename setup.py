98350291209efd18d7a02a2ebaa20783846cd912
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-04-12 11:55:02 +0200

    Code style

--- setup.py
+++ setup.py
@@ -10,28 +10,28 @@ README = open(os.path.join(here, 'README.rst')).read()
 install_requires = open(os.path.join(here, 'requirements.txt')).read().splitlines()
 
 setup(
-        name='tilecloud',
-        version=version,
-        description='Tools for managing tiles',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: BSD License',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering :: GIS',
-        ],
-        author='Tom Payne',
-        author_email='twpayne@gmail.com',
-        url='http://github.com/twpayne/tilecloud',
-        license='BSD',
-        packages=find_packages(exclude=['tiles', 'tilecloud.tests']),
-        zip_safe=True,
-        install_requires=install_requires,
-        test_suite='tilecloud.tests',
-        scripts=glob('tc-*'),
-        entry_points="""
-        # -*- Entry points: -*-
-        """,
-        long_description=README,
-        )
+    name='tilecloud',
+    version=version,
+    description='Tools for managing tiles',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+    author='Tom Payne',
+    author_email='twpayne@gmail.com',
+    url='http://github.com/twpayne/tilecloud',
+    license='BSD',
+    packages=find_packages(exclude=['tiles', 'tilecloud.tests']),
+    zip_safe=True,
+    install_requires=install_requires,
+    test_suite='tilecloud.tests',
+    scripts=glob('tc-*'),
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    long_description=README,
+)
