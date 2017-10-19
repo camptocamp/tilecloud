a1a5931b679e42bbaaf260163f43f1405ce0e1df
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-10-19 12:20:55 +0200

    Do the release 0.5.0.dev4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name='tilecloud',
-    version='0.5.0.dev3',
+    version='0.5.0.dev4',
     description='Tools for managing tiles',
     classifiers=[
         'Development Status :: 4 - Beta',
