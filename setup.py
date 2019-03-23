94e41d2f5f35b13717a4d5e81c157dafb1da735e
requires.io <support@requires.io>
2019-03-23 18:59:15 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup(
     name='tilecloud',
-    version='1.1.0',
+    version='1.2.0',
     description='Tools for managing tiles',
     classifiers=[
         'Development Status :: 4 - Beta',
