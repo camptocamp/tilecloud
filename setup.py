a8d2d1ae59cf65f71df87c4afb7eb5c64fc7e6bf
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-04-28 08:46:25 +0200

    Create release 1.6.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name="tilecloud",
-    version="1.6.0",
+    version="1.6.1",
     description="Tools for managing tiles",
     classifiers=[
         "Development Status :: 6 - Mature",
